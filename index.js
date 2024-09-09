171ee6076cb11883bacb2beabd616532eaeb9f1f
munisbek11 <munisbekshomurodov677@gmail.com>
2024-09-09 17:27:11 +0500

    added

---
+++ index.js
@@ -0,0 +1,15 @@
+// function printDiagonalSquare(size) {
+//   for (let i = 0; i < size; i++) {
+//     let row = "";
+//     for (let j = 0; j < size; j++) {
+//       if (i === 0 || i === size - 1 || j === 0 || j === size - 1 || i === j) {
+//         row += "* ";
+//       } else {
+//         row += " ";
+//       }
+//     }
+//     console.log(row);
+//   }
+// }
+
+// printDiagonalSquare(20);
